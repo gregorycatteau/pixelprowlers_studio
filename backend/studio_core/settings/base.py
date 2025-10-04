5f9913dbb4333a82b0e8cac3b050be799818a56c
gregorycatteau <contact@pixelprowlers.io>
2025-10-04 19:49:28 +0200

    chore(api): allow SessionAuthentication alongside JWT for SSR during V1

--- backend/studio_core/settings/base.py
+++ backend/studio_core/settings/base.py
@@ -140,6 +140,7 @@ REST_FRAMEWORK = {
         "rest_framework.permissions.IsAuthenticated",
     ],
     "DEFAULT_AUTHENTICATION_CLASSES": [
+        "rest_framework.authentication.SessionAuthentication",
         "rest_framework_simplejwt.authentication.JWTAuthentication",
     ],
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
